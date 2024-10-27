d2abe8125abd2652d5fff9ea2a31bcea96340710
Tarzart <exratetoxic@gmail.com>
2024-10-27 15:41:43 -0400

    final?

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,35 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello wodl!"; fart
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "media/gifs/get-this-man-a-true.gif") {
+        myImage.setAttribute("src", "media/images/therizzler.jpg");
+    } else {
+        myImage.setAttribute("src", "media/gifs/get-this-man-a-true.gif");
+    }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `MANGO, MANGO, MANGO, ${myName}`;
+    }
+  }
+  
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
